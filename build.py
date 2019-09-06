47abc089475d52296e22b448e9436b0a7ed88f08
sgalal <32255369+chromezh@users.noreply.github.com>
2019-09-06 14:20:36 +0800

    Update dictionary

--- build.py
+++ build.py
@@ -4,7 +4,7 @@
 import json
 import urllib.request
 
-source_url = 'https://raw.githubusercontent.com/sgalal/rensyuugi/b13a3ad433e863eab8c798e86a9f966eaac870ab/index.files/cantonese/data.json'
+source_url = 'https://raw.githubusercontent.com/sgalal/rensyuugi/48a31c12ca24f9d7f9c0aa62215cc060d891207b/index.files/cantonese/data.json'
 source = urllib.request.urlopen(source_url).read().decode('utf-8')
 data = json.loads(source)
 
