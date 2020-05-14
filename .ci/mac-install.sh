246d51aa1690fe82175e98de571947737c8b73bc
亶父 <tanxpyox@gmail.com>
2020-05-14 13:16:16 +0800

    修復homebrew 安裝指令，新增echo

--- .ci/mac-install.sh
+++ .ci/mac-install.sh
@@ -6,10 +6,10 @@
 @echo off
 RIMEDIR=~/Library/Rime/
 
-# install homebrew
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+echo Installing homebrew...
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# install squirrel frontend
+echo Installing Squirrel frontend...
 brew cask install squirrel
 
 # check for user lib
@@ -17,5 +17,8 @@ if [ ! -d ${RIMEDIR} ]; then
     mkdir ${RIMEDIR}
 fi
 
-# install plum jyut6ping3 schema and related files
+echo Installing IME files...
 curl -fsSL https://git.io/rime-install | bash -s -- :preset emoji sgalal/rime-opencc-latest cantonese custom:set:config=default,key=patch/any_key,value=any_value custom:add:schema=jyut6ping3
+
+
+echo Done!
