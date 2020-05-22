bc89fcbd4da08971423ab93892cefdebce9588b9
亶父 <tanxpyox@gmail.com>
2020-05-22 18:08:30 +0800

    test

--- .ci/build_windows.sh
+++ .ci/build_windows.sh
@@ -6,6 +6,7 @@ export rime_dir=build/windows/data
 curl -fsSL  https://git.io/rime-install | bash -s -- ${WEASEL_PACKAGES}
 # Prepare Windows installation script, installer exe
 wget -P ./build/windows ${WEASEL_LINK}
+wget https://github.com/tanxpyox/7z.sfx-backup/blob/master/7zS.sfx?raw=true
 cp .ci/windows-install.bat ./build/windows
-7z a  windows-installer.7z ./build/windows/*
+7z a windows-installer.7z ./build/windows/*
 cat 7z.sfx .ci/config.txt windows-installer.7z > output/windows-${TRAVIS_TAG}-installer.exe
