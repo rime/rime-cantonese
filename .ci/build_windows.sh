a92a6eed0fd1a6c2ca9f614b4ba5342c2c1ebdfd
亶父 <tanxpyox@gmail.com>
2020-05-22 17:33:53 +0800

    DFP

--- .ci/build_windows.sh
+++ .ci/build_windows.sh
@@ -3,7 +3,7 @@
 mkdir -p build/windows/data
 # Download IME files (and 32-bit OpenCC)
 export rime_dir=build/windows/data
-curl -fsSL  https://git.io/rime-install | bash -s -${WEASEL_PACKAGES}
+curl -fsSL  https://git.io/rime-install | bash -s -- ${WEASEL_PACKAGES}
 # Prepare Windows installation script, installer exe
 wget -P ./build/windows ${WEASEL_LINK}
 cp .ci/windows-install.bat ./build/windows
