b5bcde1e4f57898bfdb88cb78f356359be39bd7a
亶父 <tanxpyox@gmail.com>
2020-05-22 23:35:26 +0800

    sfx no admin permission

--- .ci/build_windows.sh
+++ .ci/build_windows.sh
@@ -7,7 +7,7 @@ curl -fsSL  https://git.io/rime-install | bash -s -- ${WEASEL_PACKAGES}
 # Prepare Windows installation script, installer exe
 wget -P ./build/windows ${WEASEL_LINK}
 cp .ci/windows-install.bat ./build/windows
-wget https://github.com/tanxpyox/7z.sfx-backup/raw/master/7zS2.sfx
+wget https://github.com/tanxpyox/7z.sfx-backup/raw/master/7zS.sfx
 7z a windows-installer.7z ./build/windows/*
 7z a output/windows-${TRAVIS_TAG}-installer.zip ./build/windows/*
-cat 7zS2.sfx .ci/config.txt windows-installer.7z > output/windows-sfx-${TRAVIS_TAG}-installer.exe
+cat 7zS.sfx .ci/config.txt windows-installer.7z > output/windows-sfx-${TRAVIS_TAG}-installer.exe
