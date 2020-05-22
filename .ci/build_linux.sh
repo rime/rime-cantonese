beedbdb1861af3b859cc14b101a62ac9814e0e6d
亶父 <tanxpyox@gmail.com>
2020-05-22 17:13:06 +0800

    declutter 2

--- .ci/build_linux.sh
+++ .ci/build_linux.sh
@@ -6,5 +6,6 @@ wget $SQUIRREL_LINK
 7z e Squirrel*.zip -obuild/mac/
 cp .ci/mac-install.sh ./build/mac
 # Build archives
-7z a output/windows-${TRAVIS_TAG}-installer.zip ./build/data ./build/windows/*
 7z a output/mac-${TRAVIS_TAG}-installer.zip ./build/mac/*
+cp .ci/ibus-install.sh output/
+cp .ci/default.custom.yaml output/
