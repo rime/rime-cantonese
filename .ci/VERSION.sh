d05211009b74d41776e86db6ce7b476a952db676
亶父 <tanxpyox@gmail.com>
2020-05-22 17:01:21 +0800

    export

--- .ci/VERSION.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set environment variables
-export WEASEL_LINK=https://dl.bintray.com/rime/weasel/weasel-0.14.3.0-installer.exe
-export SQUIRREL_LINK=https://dl.bintray.com/rime/squirrel/Squirrel-0.14.0.zip
-export WEASEL_VERSION=weasel-0.14.3
-export SQUIRREL_VERSION=squirrel-0.14.0
-export WEASEL_PACKAGES="cantonese emoji sgalal/rime-opencc-32bit-latest custom:set:config=default,key=installed_from,value=rime-cantonese custom:add:schema=jyut6ping3"
