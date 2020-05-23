d1946b0fa576374c82023dfa8145286629535fc8
亶父 <tanxpyox@gmail.com>
2020-05-23 16:11:00 +0800

    改用經簽署嘅SFX Header，降低被防毒軟件誤判做惡意軟件嘅機會

--- .ci/build_windows.sh
+++ .ci/build_windows.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 mkdir -p build/windows/data
+# Prepare installation script from env variables
 cat .ci/windows-install-template.bat | envsubst > ./build/windows/windows-install.bat
 # Download IME files (and 32-bit OpenCC)
 export rime_dir=build/windows/data
 curl -fsSL  https://git.io/rime-install | bash -s -- ${WEASEL_PACKAGES}
-# Prepare Windows installation script, installer exe
+# Prepare installer exe
 wget -P ./build/windows ${WEASEL_LINK}
-wget https://github.com/tanxpyox/7z.sfx-backup/raw/master/7zS.sfx
+#Packing...
+wget https://github.com/tanxpyox/7z.sfx-backup/raw/master/${SFXHEADER}
 7z a windows-installer.7z ./build/windows/*
 7z a output/windows-${TRAVIS_TAG}-installer.zip ./build/windows/*
-cat 7zS.sfx .ci/config.txt windows-installer.7z > output/windows-sfx-${TRAVIS_TAG}-installer.exe
+cat ${SFXHEADER} .ci/config.txt windows-installer.7z > output/windows-sfx-${TRAVIS_TAG}-installer.exe
