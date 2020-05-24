b36bf44de431f2d8dfd44a2c89f21437eb319849
亶父 <tanxpyox@gmail.com>
2020-05-24 13:23:05 +0800

    bin

--- .ci/build_android.sh
+++ .ci/build_android.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # Android packing script
 
-
+export PATH=/usr/local/android-sdk/build-tools/25.0.2/:$PATH
 export ANDROID_PACKAGE_NAME=android-${TRAVIS_TAG}.apk
 wget ${TRIME_LINK} -o output/${ANDROID_PACKAGE_NAME}
 mkdir -p assets/rime
 export rime_dir=assets/rime
-curl -fsSL  https://git.io/rime-install | bash -s -- ${WEASEL_PACKAGES}
+curl -fsSL  https://git.io/rime-install | bash -s -- ${TRIME_PACKAGES}
 aapt add output/${ANDROID_PACKAGE_NAME} $rimedir/*
-
 aapt list output/${ANDROID_PACKAGE_NAME}
