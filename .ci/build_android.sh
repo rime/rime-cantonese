366e13507857f3ae398dfa9a98932c9376b5e4e7
亶父 <tanxpyox@gmail.com>
2020-05-24 14:54:57 +0800

    update

--- .ci/build_android.sh
+++ .ci/build_android.sh
@@ -12,12 +12,16 @@ export rime_dir=assets/rime
 curl -fsSL  https://git.io/rime-install | bash -s -- ${TRIME_PACKAGES}
 cp .ci/default.custom.yaml $rime_dir
 
-
+# add IME files
 aapt add ${APK_NAME} $rime_dir/*
 aapt remove ${APK_NAME} $rime_dir/opencc/*
 aapt add ${APK_NAME} $rime_dir/opencc/*
 echo Finished adding files, check:
 aapt list ${APK_NAME}
-# Installing IME Files...
 
-# aapt2 add output/${ANDROID_PACKAGE_NAME} $rimedir/*
+# zipalign
+zipalign -f ${APK_NAME}
+
+# Sign apk
+echo y | keytool -genkeypair -dname "cn=Tanxpyox, ou=Rime-Cantonese, o=RCWorkGroup, c=HK" -alias business -keypass ${KEYPASS} -keystore ./key.ks -storepass ${STOREPASS} -validity 20000
+apksigner sign --ks key.ks $APK_NAME
