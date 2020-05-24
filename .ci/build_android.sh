0bc8ffbe67f1d66ea74e715f8858740c9c08f919
亶父 <tanxpyox@gmail.com>
2020-05-24 13:54:39 +0800

    check

--- .ci/build_android.sh
+++ .ci/build_android.sh
@@ -4,12 +4,13 @@
 # Preparing apk
 export PATH=/usr/local/android-sdk/build-tools/25.0.2/:$PATH
 export APK_NAME=output/android-${TRAVIS_TAG}.apk
-wget --output-document=${APK_NAME} ${TRIME_LINK} 
+wget --output-document=${APK_NAME} ${TRIME_LINK}
 
 # Downloading IME files
 mkdir -p assets/rime
 export rime_dir=assets/rime
 curl -fsSL  https://git.io/rime-install | bash -s -- ${TRIME_PACKAGES}
+cp .ci/defaut.custom.yaml $rime_dir
 
 aapt list ${APK_NAME}
 aapt add ${APK_NAME} $rime_dir/*
