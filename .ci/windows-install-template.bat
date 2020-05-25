dd08959cd0f3dde5e310c5b27b5f0e86e0df792f
亶父 <tanxpyox@gmail.com>
2020-05-25 10:46:52 +0800

    fix(UI): Added prompt before installing weasel frontend

--- .ci/windows-install-template.bat
+++ .ci/windows-install-template.bat
@@ -9,9 +9,8 @@ SET VERSION=weasel-${WEASEL_VERSION}
 SET RIMEPROGRAM=%ProgramFiles%\Rime\%VERSION%\
 IF %OS%==64BIT SET RIMEPROGRAM=%ProgramFiles(x86)%\Rime\%VERSION%\
 
-echo Installing Weasel frontend...
-
 SET /P TRADITIONAL=Configure Traditional Chinese (Y/[N])?
+echo Installing Weasel frontend...
 IF /I "%TRADITIONAL%" NEQ "N" (
   for %%x in (*.exe) do (%%x /S /T)
 ) else (
@@ -26,5 +25,5 @@ xcopy /E /Y data %RIMEDIR%
 echo Deploying... This may take quite a while...
 "%RIMEPROGRAM%\WeaselDeployer.exe" /deploy
 
-echo Done!
+echo Done! Please restart your computer to complete the installation.
 pause
