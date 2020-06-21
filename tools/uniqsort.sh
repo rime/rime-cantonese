36644bc950e1cdeb85db150db3a99afb54c146eb
亶父 <tanxpyox@gmail.com>
2020-06-21 22:13:52 +0800

    feature(uniqsort): improve CLI interface

--- tools/uniqsort.sh
+++ tools/uniqsort.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Remove duplicates && Sort entries BY PRONUNCIATION (if available); else, BY CHINESE CHARACTERS
 
 # Base list--to be 'uniquified'
-export candidates=(
+candidates=(
   jyut6ping3.dict.yaml
   jyut6ping3.lettered.dict.yaml
   jyut6ping3.phrase.dict.yaml
 )
 
-# Add files to this list if it is ALSO READY TO BE SORTED
-export goodToSort=(
+# Add files to this list if they are ALSO READY TO BE SORTED
+goodToSort=(
   jyut6ping3.phrase.dict.yaml
   # Do not use this script on the following dictionaries, as they contain
   # new entries that are not yet good to merge
@@ -17,14 +17,25 @@ export goodToSort=(
   ## jyut6ping3.lettered.dict.yaml
 )
 
+# Override Win32 sort.exe if on Git Bash
 if [ $OSTYPE=="msys" ]; then
-  export PATH="/usr/bin:$PATH"
+  PATH="/usr/bin:$PATH"
 fi
+
+# Set sort order by locale (default: "C"=radical-stroke)
+LC_ALL="C"
 
 for file in ${candidates[@]}
 do
+  echo ===============================
+  echo $file
   awk '{if(!x["..."] && $0 != "...") {print > "a.tmp";} else if($0 == "...") {print > "a.tmp";x["..."]++;} else if($0=="" || !x[$0]++) {print > "b.tmp"}}' $file
-  if [ $(echo $goodToSort | grep -w $file) ]; then sort -k2,2 -k1,1 -o b.tmp b.tmp; fi
+  echo  "  : uniquified"
+  if [ $(echo $goodToSort | grep -w $file) ]; then
+    sort -k2,2 -k1,1 -o b.tmp b.tmp
+    echo "  : sorted"
+  fi
   cat a.tmp b.tmp > $file
   rm a.tmp b.tmp
+  echo  "  - changes merged"
 done
