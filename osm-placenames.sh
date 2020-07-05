71ff6871f86019d3046004869fbdf61e3a4434a2
szc126 <29127915+szc126@users.noreply.github.com>
2020-07-04 17:07:24 -0700

    style(osm-placenames) -remove `main()` wrapper function (introduced for `local`, removed in fec1e6c8d1350fe0680a2d983950f577f4dc1c9b)

--- osm-placenames.sh
+++ osm-placenames.sh
@@ -42,37 +42,33 @@ use_preset_vocabulary: true
 ...
 "
 
-main () {
-	for ((i = 0; i <= $((queries_len - 1)); i += 2))
-	do
-		name=${queries[${i}]}
-		query=${queries[$((i + 1))]}
-		url=$(printf "${overpass_api}" "${query}")
-		echo "${name}"
-		echo "${query}"
-		echo "----"
+for ((i = 0; i <= $((queries_len - 1)); i += 2))
+do
+	name=${queries[${i}]}
+	query=${queries[$((i + 1))]}
+	url=$(printf "${overpass_api}" "${query}")
+	echo "${name}"
+	echo "${query}"
+	echo "----"
 
-		wget_temp_out=$(mktemp)
-		echo "${wget_temp_out}"
-		wget "${url}" -O "${wget_temp_out}"
-		echo "----"
+	wget_temp_out=$(mktemp)
+	echo "${wget_temp_out}"
+	wget "${url}" -O "${wget_temp_out}"
+	echo "----"
 
-		cut_temp_out=$(mktemp)
-		echo "${cut_temp_out}"
-		cut "${wget_temp_out}" --fields=1,2,3 --output-delimiter=$'\n' | sort | uniq > "${cut_temp_out}"
-		echo "----"
+	cut_temp_out=$(mktemp)
+	echo "${cut_temp_out}"
+	cut "${wget_temp_out}" --fields=1,2,3 --output-delimiter=$'\n' | sort | uniq > "${cut_temp_out}"
+	echo "----"
 
-		cjk_range=$'\u4e00-\u9fff'
-		# must be LANG=C. en_US.UTF-8: grep does not like \u9fff.
-		LANG=C grep -E "[^${cjk_range}]" "${cut_temp_out}" | less
-		echo "----"
+	cjk_range=$'\u4e00-\u9fff'
+	# must be LANG=C. en_US.UTF-8: grep does not like \u9fff.
+	LANG=C grep -E "[^${cjk_range}]" "${cut_temp_out}" | less
+	echo "----"
 
-		out="/tmp/"$(printf "${rime_dictionary_file_name}" "${name}")
-		echo "${out}"
-		printf "${rime_dictionary_header}" "${name}" "${date}" >> "${out}"
-		cat "${cut_temp_out}" >> "${out}"
-		echo "----"
-	done
-}
-
-main
+	out="/tmp/"$(printf "${rime_dictionary_file_name}" "${name}")
+	echo "${out}"
+	printf "${rime_dictionary_header}" "${name}" "${date}" >> "${out}"
+	cat "${cut_temp_out}" >> "${out}"
+	echo "----"
+done
