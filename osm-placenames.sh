8b4990f7356c4f3a6f6cd1c9e849393954bd9f30
szc126 <29127915+szc126@users.noreply.github.com>
2020-07-04 17:05:22 -0700

    style(osm-placenames) -`local` does not work as expected -$queries_len represents the actual length per its name

--- osm-placenames.sh
+++ osm-placenames.sh
@@ -17,7 +17,7 @@ queries=(
 		.myWays out tags;
 	"
 )
-queries_len=$((${#queries[@]} - 1))
+queries_len=${#queries[@]}
 rime_dictionary_file_name="jyut6ping3.%s.dict.yaml"
 rime_dictionary_header="# Rime dictionary
 # encoding: utf-8
@@ -43,34 +43,34 @@ use_preset_vocabulary: true
 "
 
 main () {
-	for ((i = 0; i <= queries_len; i += 2))
+	for ((i = 0; i <= $((queries_len - 1)); i += 2))
 	do
-		local name=${queries[${i}]}
-		local query=${queries[$((i + 1))]}
-		local url=$(printf "${overpass_api}" "${query}")
+		name=${queries[${i}]}
+		query=${queries[$((i + 1))]}
+		url=$(printf "${overpass_api}" "${query}")
 		echo "${name}"
 		echo "${query}"
 		echo "----"
 
-		local wget_temp_out=$(mktemp)
+		wget_temp_out=$(mktemp)
 		echo "${wget_temp_out}"
 		wget "${url}" -O "${wget_temp_out}"
 		echo "----"
 
-		local cut_temp_out=$(mktemp)
+		cut_temp_out=$(mktemp)
 		echo "${cut_temp_out}"
 		cut "${wget_temp_out}" --fields=1,2,3 --output-delimiter=$'\n' | sort | uniq > "${cut_temp_out}"
 		echo "----"
 
-		local cjk_range=$'\u4e00-\u9fff'
+		cjk_range=$'\u4e00-\u9fff'
 		# must be LANG=C. en_US.UTF-8: grep does not like \u9fff.
-		LANG=C grep -E "[^${cjk_range}]" "${cut_temp_out}" | less 
+		LANG=C grep -E "[^${cjk_range}]" "${cut_temp_out}" | less
 		echo "----"
 
-		local rime_dictionary_file_name="/tmp/"$(printf "${rime_dictionary_file_name}" "${name}")
-		echo "${rime_dictionary_file_name}"
-		printf "${rime_dictionary_header}" "${name}" "${date}" >> "${rime_dictionary_file_name}"
-		cat "${cut_temp_out}" >> "${rime_dictionary_file_name}"
+		out="/tmp/"$(printf "${rime_dictionary_file_name}" "${name}")
+		echo "${out}"
+		printf "${rime_dictionary_header}" "${name}" "${date}" >> "${out}"
+		cat "${cut_temp_out}" >> "${out}"
 		echo "----"
 	done
 }
