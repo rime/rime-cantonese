d20f78a0a848c31a3de5208675fa50d47091a28d
sgalal <sgalal.me@outlook.com>
2020-07-10 21:49:51 +0800

    Upload result

---
+++ build.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def load_dict(path):
+	with open(path) as f:
+		return {k: int(v) for line in f for k, v in (line.rstrip().split('\t'),)}
+
+def write_dict(d, path):
+	with open(path, 'w') as f:
+		for k, v in d.items():
+			print(k, v, sep='\t', file=f)
+
+a = load_dict('essay.txt')
+b = load_dict('cifu-freq.txt')
+b = {k: int(v * 4822928 / 12448) for k, v in b.items()}  # ref: freq('的')
+
+write_dict({**a, **b}, 'essay-cantonese.txt')
