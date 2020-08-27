6b317e53fbc3c756b53c502691c1df5df936960a
szc126 <29127915+szc126@users.noreply.github.com>
2020-08-27 10:44:59 -0700

    Update OSM script (#92)
    * create script for retrieving placenames from OpenStreetMap data
    see #69
    
    * fix(osm-placenames): suppress CSV header
    
    * fix(osm-placenames): remove unnecessary command from query
    remove 香港 from output data
    
    * style(osm-placenames)
    -`local` does not work as expected
    -$queries_len represents the actual length per its name
    
    * style(osm-placenames)
    -remove `main()` wrapper function (introduced for `local`, removed in fec1e6c8d1350fe0680a2d983950f577f4dc1c9b)
    
    * style(osm-placenames)
    
    * fix(osm-placenames): name processing
    - handle multiple names in one "name" field separated by a semicolon
    - sort as LANG=C
    
    * feat(osm-placenames): ignore ways with certain attributes
    
    * feat(osm-placenames): s/－.+段//g and add comment regarding sed
    
    * refactor(osm-placenames): change `sort | uniq > OUT` to `sort --unique --out=OUT`
    
    * style(osm-placenames)
    
    * style(osm-placenames): add comments
    
    * feat(osm-placename): remove "name:en" and "id"
    not important
    
    * feat(osm-placenames): modify for portability
    modify for OS X
    
    * fix(osm-placenames): YYYY.MM.DD
    
    * style(osm-placenames.sh): expand header comment
    - add overpass turbo
    - add explanation
    
    * feat(osm-placenames.sh): init Macau, Guangdong, Guangxi
    
    * fix(osm-placenames.sh): output to working directory
    rime/rime-cantonese#76
    
    * style(osm-placenames): inline $cjk_range
    
    * style(osm_placenames.sh):
    - move variable declaration to the top
    - with color print a query name
    - "BMP first set"→"基本區"
    
    * feat(osm_placenames.sh): save lines with 非漢字 to tmp
    
    * osm-placenames.awk: init
    
    * feat(osm-placenames.sh): exit on error
    set -e
    
    * feat(osm-placenames.sh): conversion from Simplified to Traditional Chinese
    
    * style(osm-placenames.sh)
    
    * chore(osm-placenames.sh): update dictionary header
    
    * osm-placenames.awk: +
    
    * osm-placenames.sh:
    - style: rename $XXX_tmp_out to $XXX_out
    - style: rename wget→dl, cut→tidy, grep_nonHani→non_Hani
    - style: use $return to keep track of the last $XXX_out
    - style: modify comments
    - write opencc diff to file
    - incorporate osm-placenames.awk
    - add user agent to wget
    - comment-out `set -e` (opencc return code?)
    
    Co-authored-by: 亶父 (taan² fu²) <tanxpyox@gmail.com>

---
+++ osm-placenames.awk
@@ -0,0 +1,26 @@
+#awk -f osm-placenames.awk /tmp/rime-cantonese.osm_hk_ways.cut.
+
+BEGIN {
+	# 讀音
+	rs["昂坪360救援徑"] = "ngong4 ping4 saam1 luk6 ling4 gau3 wun4 ging3/ngong5 ping4 saam1 luk6 ling4 gau3 wun4 ging3";
+}
+
+# 含非基本區字
+/[^一-鿿]/ {
+	# 若無其讀音
+	if ($0 in rs == 0) {
+		# 除之
+		next;
+	}
+}
+
+{
+	if ($0 in rs) {
+		split(rs[$0], rs_0, "/");
+		for (r in rs_0) {
+			print $0 "\t" rs_0[r];
+		}
+	} else {
+		print $0;
+	}
+}
--- osm-placenames.sh
+++ osm-placenames.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+#set -e
 
-# https://github.com/rime/rime-cantonese/issues/69
+# request for HK street names:
+#   https://github.com/rime/rime-cantonese/issues/69
+# test queries at:
+#   https://overpass-turbo.eu/
+#   (must unescape quotation marks)
 
 overpass_api="https://lz4.overpass-api.de/api/interpreter?data=%s"
 queries=(
@@ -28,22 +33,93 @@ queries=(
 
 		.myWays out tags;
 	"
+
+	"osm_mo_ways"
+	"
+		[out:csv(\"name:zh\", \"alt_name:zh\", \"old_name:zh\"; false)][timeout:25];
+
+		relation(1867188);map_to_area->.mySearchArea;
+		(
+			way
+				[\"highway\"]
+				[\"highway\"!=\"bus_stop\"]
+				[\"highway\"!=\"construction\"]
+				[\"highway\"!=\"corridor\"]
+				[\"highway\"!=\"proposed\"]
+				[\"highway\"!=\"steps\"]
+				[\"access\"!=\"private\"]
+				[\"area\"!=\"yes\"]
+				[\"name:zh\"]
+				(area.mySearchArea)
+			;
+		)->.myWays;
+
+		.myWays out tags;
+	"
+
+	"osm_cn-gd_ways"
+	"
+		[out:csv(\"name:zh\", \"alt_name:zh\", \"old_name:zh\"; false)][timeout:25];
+
+		relation(911844);map_to_area->.mySearchArea;
+		(
+			way
+				[\"highway\"]
+				[\"highway\"!=\"bus_stop\"]
+				[\"highway\"!=\"construction\"]
+				[\"highway\"!=\"corridor\"]
+				[\"highway\"!=\"proposed\"]
+				[\"highway\"!=\"steps\"]
+				[\"access\"!=\"private\"]
+				[\"area\"!=\"yes\"]
+				[\"name:zh\"]
+				(area.mySearchArea)
+			;
+		)->.myWays;
+
+		.myWays out tags;
+	"
+
+	"osm_cn-gx_ways"
+	"
+		[out:csv(\"name:zh\", \"alt_name:zh\", \"old_name:zh\"; false)][timeout:25];
+
+		relation(286342);map_to_area->.mySearchArea;
+		(
+			way
+				[\"highway\"]
+				[\"highway\"!=\"bus_stop\"]
+				[\"highway\"!=\"construction\"]
+				[\"highway\"!=\"corridor\"]
+				[\"highway\"!=\"proposed\"]
+				[\"highway\"!=\"steps\"]
+				[\"access\"!=\"private\"]
+				[\"area\"!=\"yes\"]
+				[\"name:zh\"]
+				(area.mySearchArea)
+			;
+		)->.myWays;
+
+		.myWays out tags;
+	"
 )
 queries_len=${#queries[@]}
-cjk_range=$'\u4e00-\u9fff'
+
 date=$(date "+%Y.%m.%d")
-rime_dictionary_file_name="/tmp/jyut6ping3.%s.dict.yaml"
+rime_dictionary_file_name="./jyut6ping3.%s.dict.yaml"
 rime_dictionary_header="# Rime dictionary
 # encoding: utf-8
 #
 # Jyut6ping3 - 粵拼詞庫
 #
 # 碼表製作與校對人員
-# sgal <ayaka@mail.shn.hk>
+# sgalal <sgalal.me@outlook.com>
 # chaaklau <chaakming@gmail.com>
 # LeiMaau <leimaau@qq.com>
 # laubonghaudoi <laubonghaudoi@icloud.com>
 # tanxpyox <tanxpyox@gmail.com>
+# William915 <William915@gmail.com>
+# szc126 (@szc126)
 #
 # Data © OpenStreetMap and its contributors.
 # Data licensed under the Open Data Commons Open Database License (ODbL).
@@ -55,43 +131,77 @@ sort: by_weight
 use_preset_vocabulary: true
 ...
 "
+opencc_config="/usr/share/opencc/s2t.json"
 
 for ((i = 0; i <= $((queries_len - 1)); i += 2))
 do
 	name=${queries[${i}]}
 	query=${queries[$((i + 1))]}
 	url=$(printf "${overpass_api}" "${query}")
-	echo "${name}"
+
+	dl_out=$(mktemp -t "rime-cantonese.${name}.dl.$$.XXXX")
+	tidy_out=$(mktemp -t "rime-cantonese.${name}.tidy.$$.XXXX")
+	nonHani_out=$(mktemp -t "rime-cantonese.${name}.nonHani.$$.XXXX")
+	awk_out=$(mktemp -t "rime-cantonese.${name}.awk.$$.XXXX")
+	opencc_out=$(mktemp -t "rime-cantonese.${name}.opencc.$$.XXXX")
+	opencc_diff_out=$(mktemp -t "rime-cantonese.${name}.openccDiff.$$.XXXX")
+	out=$(printf "${rime_dictionary_file_name}" "${name}")
+
+	return=""
+
+	# ----
+
+	printf "\\e[97;46m${name}\\e[0m\n"
 	echo "${query}"
 	echo "----"
 
 	# download OpenStreetMap data from the Overpass API
-	wget_temp_out=$(mktemp -t "rime-cantonese.${name}.wget.$$.XXXX")
-	echo "${wget_temp_out}"
-	wget "${url}" -O "${wget_temp_out}"
+	echo "${dl_out}"
+	wget "${url}" -O "${dl_out}" --user-agent="https://github.com/rime/rime-cantonese"
 	echo "----"
+	return="${dl_out}"
 
 	# sed: convert tab and semicolon to newline
-	#      '衛奕信徑;港島徑'→ '衛奕信徑' '港島徑'
+	#      '衛奕信徑;港島徑'→'衛奕信徑' '港島徑'
 	# sed: '青山公路－荃灣段'→'青山公路'
 	# sort: sort and uniquify
-	cut_temp_out=$(mktemp -t "rime-cantonese.${name}.cut.$$.XXXX")
-	echo "${cut_temp_out}"
-	cat "${wget_temp_out}" \
+	echo "${tidy_out}"
+	cat "${return}" \
 		| sed "s/[\t;]/\n/g" \
 		| sed -E "s/－.+段//g" \
-		| LANG=C sort -u -o "${cut_temp_out}"
+		| LANG=C sort -u -o "${tidy_out}"
 	echo "----"
+	return="${tidy_out}"
 
-	# print names that contain non-漢字 (BMP first set)
+	# print entries that contain non-基本區 characters
 	# LANG=C is needed. en_US.utf8→grep does not like \u9fff.
-	LANG=C grep -E "[^${cjk_range}]" "${cut_temp_out}" | less
+	echo "${nonHani_out}"
+	LANG=C grep -E [^$'\u4e00'-$'\u9fff'] "${return}" > "${nonHani_out}"
+	less "${nonHani_out}"
 	echo "----"
+	#return=
 
-	# save complete list to $out
-	out=$(printf "${rime_dictionary_file_name}" "${name}")
+	# add readings
+	# remove entries that contain non-基本區 characters
+	echo "${awk_out}"
+	awk -f osm-placenames.awk "${return}" > "${awk_out}"
+	echo "----"
+	return="${awk_out}"
+
+	# convert with OpenCC
+	# print difference
+	echo "${opencc_out}"
+	echo "${opencc_diff_out}"
+	echo "${opencc_config}"
+	opencc --input "${return}" --config "${opencc_config}" --output "${opencc_out}"
+	diff --side-by-side --suppress-common-lines "${return}" "${opencc_out}" > "${opencc_diff_out}"
+	less "${opencc_diff_out}"
+	echo "----"
+	return="${opencc_out}"
+
+	# write dictionary to $out
 	echo "${out}"
 	printf "${rime_dictionary_header}" "${name}" "${date}" >> "${out}"
-	cat "${cut_temp_out}" >> "${out}"
+	cat "${return}" >> "${out}"
 	echo "----"
 done
