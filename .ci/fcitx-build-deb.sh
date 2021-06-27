3479d3e1075110a869e599115ce46e38146d2491
Fafofafoy <ihcaoe@gmail.com>
2021-06-27 14:28:18 +0800

    Scripts for deb.

---
+++ .ci/fcitx-build-deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## Build deb package for ubuntu Linux.user.
+
+# Clear old pkg content
+rm -rf deb/
+
+# Create temp folder to collect files
+mkdir -p deb/DEBIAN/
+mkdir -p deb/usr/share/rime-data/
+# deb package related file
+cp ./fcitx-deb/control ./fcitx-deb/postinst ./fcitx-deb/postrm deb/DEBIAN/
+# taigi
+cp ../*.yaml deb/usr/share/rime-data/
+# copyright
+cat ../LICENSE* > deb/DEBIAN/copyright
+
+# Build pkg
+dpkg -b deb/ output/rime-cantonese.deb
+
