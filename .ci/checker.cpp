a2ee8096daf55ff82278a1839ec43c406ab9ffac
亶父 <tanxpyox@gmail.com>
2021-07-06 18:00:23 +0800

    added jyutping decomposer

--- .ci/checker.cpp
+++ .ci/checker.cpp
@@ -6,13 +6,38 @@
 #include <regex>
 #include <algorithm>
 #include <cstdio>
+#define JYUT phonemes[0]
+#define INITIAL phonemes[1]
+#define STEM phonemes[2]
+#define FINAL phonemes[3]
+#define TONER phonemes[4]
 using namespace std;
 
 bool valid_jyutping(string a){
-	//is alphanumeric
-	if (!regex_match(a, (regex) "[a-z]+[1-6]")) return false;
+	smatch phonemes;
 	
-	//Add more regex rules here
+	//has a jyutping-like structure; if cannot be decomposed exactly into 4 groups, treat as invalid
+	if (!regex_match(a, phonemes, (regex) "(gw?|kw?|ng?|sh?|[bpmfdtlhzcjw])?(aa?|eo?|oe?|[iu]|yu|ng|m)([iumptk]|ng?)?([1-6])")) return false;
+	
+	/* Note: after the first regex_match, the jyutping string will be decomposed into phonemes acc to the following:
+		phonemes[0] = JYUT = full jyutping string
+		phonemes[1] = INITIAL = initial (empty if none)
+		phonemes[2] = STEM = stem (aa / a / e / i / o / u / eo / oe / yu / ng / m)
+		phonemes[3] = FINAL = final (empty if none; including -i and -u; thus -aai will be treated as -aa + -i)
+		phonemes[4] = TONE = tone 
+	*/
+	
+	
+	/* ------ CHECKING RULES ------ */
+	
+	// eo followed by t / i / n
+	if (STEM == "eo") {
+		if (!(FINAL == "t" || FINAL == "i" || FINAL == "n")) return false;
+	}
+	
+	// catch failed matches of /m/ and /ng/
+	if ((STEM == "m" || STEM == "ng") && (INITIAL != "" && INITIAL != "h")) return false;
+	if ((STEM == "m" || STEM == "ng") && FINAL != "") return false;
 	
 	return true;
 }
@@ -47,7 +72,6 @@ int parse(){
 		line_no++;
 		string word =        line.substr(0,                 line.find('\t'));
 		string jyut_string = line.substr(line.find('\t')+1, line.find('\t', line.find('\t')+1) - line.find('\t'));
-		
 		if (!process(jyut_string)) {
 			cerr << "[L" << line_no << "] Invalid Jyutping detected: " << jyut_string << endl;
 			count_invalid++;
