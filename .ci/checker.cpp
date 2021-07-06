6efdb11328f5c79457ff7a5f65e41acb99133d2f
亶父 (taan² fu²) <tanxpyox@gmail.com>
2021-07-06 19:26:43 +0800

    CI: add documentation

--- .ci/checker.cpp
+++ .ci/checker.cpp
@@ -10,31 +10,40 @@
 #define INITIAL phonemes[1]
 #define STEM phonemes[2]
 #define FINAL phonemes[3]
-#define TONER phonemes[4]
+#define TONE phonemes[4]
 using namespace std;
 
+string word, line, dict;
+smatch phonemes;
+
+// CHANGE THIS -- a function to check if a jyutping word is valid 
 bool valid_jyutping(string a){
-	smatch phonemes;
-	
-	//has a jyutping-like structure; if cannot be decomposed exactly into 4 groups, treat as invalid
+
+	// Has a jyutping-like structure -- catches most invalid spellings ('ch-', 'eu-', etc)
+	//      If cannot be decomposed into exactly 4 groups, treat as invalid
 	if (!regex_match(a, phonemes, (regex) "(gw?|kw?|ng?|sh?|[bpmfdtlhzcjw])?(aa?|eo?|oe?|[iu]|yu|ng|m)([iumptk]|ng?)?([1-6])")) return false;
 	
 	/* Note: after the first regex_match, the jyutping string will be decomposed into phonemes acc to the following:
 		phonemes[0] = JYUT = full jyutping string
+		--------------------------------
 		phonemes[1] = INITIAL = initial (empty if none)
-		phonemes[2] = STEM = stem (aa / a / e / i / o / u / eo / oe / yu / ng / m)
+		phonemes[2] = STEM = stem (aa / a / e / i / o / u / eo / oe / yu / ng* / m*)
 		phonemes[3] = FINAL = final (empty if none; including -i and -u; thus -aai will be treated as -aa + -i)
 		phonemes[4] = TONE = tone 
+
+		*Only applies to /ng/ and /m/
 	*/
 	
-	
-	/* ------ CHECKING RULES ------ */
+	/* ------ ADDITIONAL CHECKING RULES ------ */
 	
 	// eo followed by t / i / n
-	if (STEM == "eo") {
-		if (!(FINAL == "t" || FINAL == "i" || FINAL == "n")) return false;
-	}
+	if (STEM == "eo" && !(FINAL == "t" || FINAL == "i" || FINAL == "n")) return false;
 	
+	// i / yu must begin with an initial
+	if (INITIAL == "" && (STEM == "i" || STEM == "yu")) return false;
+
+	// Add more rules here...
+
 	// catch failed matches of /m/ and /ng/
 	if ((STEM == "m" || STEM == "ng") && (INITIAL != "" && INITIAL != "h")) return false;
 	if ((STEM == "m" || STEM == "ng") && FINAL != "") return false;
@@ -42,10 +51,10 @@ bool valid_jyutping(string a){
 	return true;
 }
 
-//Strips down a jyutping string into words
+//Strips down a jyutping string into words and passes to valid_jyutping() for checking
+//Can probably optimise
 bool process(string a){
 	istringstream sin(a);
-	string word;
 	
 	while(sin >> word) {
 		if (!valid_jyutping(word)) return false;
@@ -54,9 +63,8 @@ bool process(string a){
 	return true;
 }
 
-//Processes file
+//Processes file and keeps track of discovered errors
 int parse(){
-	string line;
 	int count_invalid = 0;
 	int line_no = 0;
 	
@@ -70,8 +78,8 @@ int parse(){
 	// Real checking
 	while(getline(cin, line)){
 		line_no++;
-		string word =        line.substr(0,                 line.find('\t'));
-		string jyut_string = line.substr(line.find('\t')+1, line.find('\t', line.find('\t')+1) - line.find('\t'));
+		int temp = line.find('\t');
+		string jyut_string = line.substr(temp+1, line.find('\t', temp + 1) - temp);
 		if (!process(jyut_string)) {
 			cerr << "[L" << line_no << "] Invalid Jyutping detected: " << jyut_string << endl;
 			count_invalid++;
@@ -81,8 +89,10 @@ int parse(){
 	return count_invalid;
 }
 
+// Wrapper for command line interface
 int main (int argc, char** argv) {
-	string dict = "";
+	
+	ios_base::sync_with_stdio();
 	
 	if (argc == 2){
 		dict = argv[1];
@@ -93,6 +103,6 @@ int main (int argc, char** argv) {
 		return 1;
 	}
 	
-	freopen(dict.c_str(), "r", stdin ); // redirect file to stdin stream
+	freopen(dict.c_str(), "r", stdin); // redirect file to stdin stream
 	return parse();
 }
