0531a2855c44ad77c339c7b4f75839ab2069eceb
tanxpyox <tanxpyox@gmail.com>
2023-05-03 12:52:50 +0000

    Deploying to master from @ rime/rime-cantonese@b57711e5f27d326b48a596a078f59ed085672ed4 🚀

--- .ci/build_linux.sh
+++ .ci/build_linux.sh
@@ -8,12 +8,12 @@ set -e
 mkdir -p build/mac
 
 # Download Mac installation script, Mac package and unzip
-wget $SQUIRREL_LINK
+wget ${SQUIRREL_LINK}
 7z e Squirrel*.zip -obuild/mac/
 cat .ci/mac-install-template.sh | envsubst '${SQUIRREL_PACKAGES}' > ./build/mac/mac-install.sh
 
 # Build archives
-7z a output/mac-${TRAVIS_TAG}-installer.zip ./build/mac/*
+7z a output/mac-${GITHUB_REF_NAME}-installer.zip ./build/mac/*
 
 # Copy default.custom.yaml and ibus-install.sh into output queue
 cat .ci/ibus-install-template.sh | envsubst '${IBUS_PACKAGES}' > ./output/ibus-install.sh
