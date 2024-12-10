7648c5f774cd151e0e5eef80dc24b712a1849bf3
graphemecluster <graphemecluster@gmail.com>
2024-12-10 17:06:56 +0800

    Try TypeDuck schema

--- .ci/install-schema.sh
+++ .ci/install-schema.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-curl -fsSL https://git.io/rime-install | bash -s -- prelude
+git clone https://github.com/TypeDuck-HK/schema "$rime_dir"
 
 if [ -n "$rime_dir" ]; then
   pushd "$rime_dir"
-  perl -0777 -i -pe 's/\nschema_list:\n.*?\n\n/
-installed_from: rime-cantonese
-
-schema_list:
-  - schema: jyut6ping3
-  - schema: cangjie5
-  - schema: stroke
-  - schema: luna_pinyin
-
-/s; s/page_size: [0-9]+/page_size: 8/' default.yaml
-  rm emoji_cantonese_suggestion.yaml
+  rm -rf .git .gitignore trime.yaml jyut6ping3_mobile*.yaml *_longpress*.yaml *.custom.yaml
   popd
 fi
